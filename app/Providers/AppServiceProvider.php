d676819fc9fc563d60f9e1a189acb8164a7df44e
Giacomo Gasperini <giacomo.gasperini@gmail.com>
2026-01-23 11:12:36 +0100

    mail 11

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Events\MessageSending;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +30,17 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
+
+    Mail::beforeSending(function (MessageSending $event) {
+        \Log::info('MAIL CONFIG:', [
+            'host' => config('mail.mailers.smtp.host'),
+            'port' => config('mail.mailers.smtp.port'),
+            'username' => config('mail.mailers.smtp.username'),
+            'password' => substr(config('mail.mailers.smtp.password'), 0, 10) . '...',
+            'encryption' => config('mail.mailers.smtp.encryption'),
+            'from' => config('mail.from'),
+        ]);
+    });
+
     }
 }
