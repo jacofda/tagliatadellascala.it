ecba289b2f5a9d488375fd711e68fe79ee10cb14
Giacomo Gasperini <giacomo.gasperini@gmail.com>
2026-01-23 10:51:57 +0100

    mail 10

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Mail;
-use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
-use Symfony\Component\Mailer\Transport\Smtp\Stream\SocketStream;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,31 +27,5 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
-
-        Mail::extend('custom_smtp', function ($config) {
-            $streamOptions = [
-                'ssl' => [
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                    'allow_self_signed' => true,
-                ],
-            ];
-            
-            $transport = new EsmtpTransport(
-                $config['host'],
-                $config['port'],
-                $config['encryption'] ?? false
-            );
-            
-            $transport->setStreamOptions($streamOptions);
-            
-            if (isset($config['username'])) {
-                $transport->setUsername($config['username']);
-                $transport->setPassword($config['password']);
-            }
-            
-            return $transport;
-        });
-
     }
 }
--- config/mail.php
+++ config/mail.php
@@ -35,12 +35,14 @@ return [
 
     'mailers' => [
         'smtp' => [
-            'transport' => 'custom_smtp',  // <-- cambia questo
-            'host' => env('MAIL_HOST'),
-            'port' => env('MAIL_PORT'),
-            'encryption' => env('MAIL_ENCRYPTION'),
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
         'ses' => [
