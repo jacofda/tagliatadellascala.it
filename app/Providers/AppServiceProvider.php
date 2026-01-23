7bc9c81cf1e5488110877b41e29668a4f5e9cd36
Giacomo Gasperini <giacomo.gasperini@gmail.com>
2026-01-23 14:08:54 +0100

    FIX FPC

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Mail\Events\MessageSending;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -30,17 +28,6 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
-
-    Mail::beforeSending(function (MessageSending $event) {
-        \Log::info('MAIL CONFIG:', [
-            'host' => config('mail.mailers.smtp.host'),
-            'port' => config('mail.mailers.smtp.port'),
-            'username' => config('mail.mailers.smtp.username'),
-            'password' => substr(config('mail.mailers.smtp.password'), 0, 10) . '...',
-            'encryption' => config('mail.mailers.smtp.encryption'),
-            'from' => config('mail.from'),
-        ]);
-    });
 
     }
 }
